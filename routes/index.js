2be86e51c9c90be47a93d9934f9e8d893f2822f5
IgnacioEsparza <v.esparza02@ufromail.cl>
2019-08-01 18:09:22 -0400

    Avance Backend

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express = require('express');
+
+const api = express.Router();
+const auth = require('../middleware/auth')
+
+const propuestaCtrl = require('../controllers/propuesta');
+const userCtrl = require('../controllers/user')
+
+api.get('/propuesta', propuestaCtrl.getPropuestas);
+api.get('/propuesta/:propuestaId', propuestaCtrl.getPropuesta);
+api.post('/propuesta', propuestaCtrl.savePropuesta);
+api.put('/propuesta/:propuestaId', propuestaCtrl.updatePropuesta);
+api.delete('/propuesta/:propuestaId', propuestaCtrl.deletePropuesta);
+
+api.post('/signup', userCtrl.signUp);
+api.post('/login', userCtrl.signIn);
+api.get('/private', auth, function (req, res) {
+    res.status(200).send({menssage: 'Autenticado correctamente'})
+})
+
+module.exports = api;
